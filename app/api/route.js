6040a3cf936da3e0c3ae38b194ffcca38cb788ec
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2024-01-04 21:08:43 +0100

    Connect to MongoDB

--- app/api/route.js
+++ app/api/route.js
@@ -3,6 +3,3 @@ import { NextResponse } from 'next/server';
 export async function GET(req) {
   return NextResponse.json({ time: new Date().toLocaleString() });
 }
-
-// DtOdlvuWDr061lxi
-// mongodb+srv://zibi:DtOdlvuWDr061lxi@cluster0.owlfbi3.mongodb.net/?retryWrites=true&w=majority
