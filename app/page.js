a578618ce72dead8b14dcd626622805e062682fb
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2024-01-04 16:58:28 +0100

    Instaling bootstrap-material-design

--- app/page.js
+++ app/page.js
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <h1 className='d-flex justify-content-center align-items-center vh-100 text-uppercase'>
+      Home
+    </h1>
+  );
 }
